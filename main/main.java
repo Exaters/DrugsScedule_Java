a6edb192ac99c970e14c770dda85bb8ebefbdc0f
Exate <144182792+Exaters@users.noreply.github.com>
2025-05-30 13:21:41 +0300

    Create project structure
    Add pacages and main files

---
+++ main/main.java
@@ -0,0 +1,11 @@
+package main;
+import javafx.application.Application;
+
+public class main {
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(app.class, args);
+    }
+}
